155ca721a55874b445cf35b31dba5dd00079c9ad
RahulSriram <rahulrockingsriram@gmail.com>
2016-04-04 10:02:04 +0530

    bug fixes

--- src/libman/BorrowDetails.java
+++ src/libman/BorrowDetails.java
@@ -12,9 +12,9 @@ package libman;
 public class BorrowDetails extends javax.swing.JFrame {
 
     /**
-     * Creates new form borrowDetails
+     * Creates new form BorrowDetails
      */
-    public borrowDetails() {
+    public BorrowDetails() {
         initComponents();
     }
 
@@ -143,20 +143,20 @@ public class BorrowDetails extends javax.swing.JFrame {
         try {
 		util.setLookAndFeel();
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(borrowDetails.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(BorrowDetails.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(borrowDetails.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(BorrowDetails.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(borrowDetails.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(BorrowDetails.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(borrowDetails.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(BorrowDetails.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new borrowDetails().setVisible(true);
+                new BorrowDetails().setVisible(true);
             }
         });
     }
