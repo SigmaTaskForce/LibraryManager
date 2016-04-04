2f032ed63906ced6dbc8df21ddabd8c624d8ca58
RahulSriram <rahulrockingsriram@gmail.com>
2016-04-05 02:14:43 +0530

    fixes to csv formatting

--- scripts/csvImporter.java
+++ scripts/csvImporter.java
@@ -25,23 +25,25 @@ public class csvImporter {
 		JFileChooser filechooser = new JFileChooser();
 		filechooser.showOpenDialog(null);
 		File file = filechooser.getSelectedFile();
-		
+
 		try(Scanner sc = new Scanner(file)) {
 			sc.nextLine();
 
-			while(sc.hasNextLine()) {
-				String[] col = sc.nextLine().split("|");
+			SQLUpdate("CREATE DATABASE Library");
+			SQLUpdate("Library","CREATE TABLE BookDetails(AccNo text,Title text,Publisher text,Domain text,Year int,Price int)");
+			SQLUpdate("Library","CREATE TABLE Author(AccNo text,AuthorName text)");
 
-				SQLUpdate("CREATE DATABASE Library");
-				SQLUpdate("Library","CREATE TABLE BookDetails(AccNo text,Title text,Publisher text,Domain text,Year int,Price int,Edition text)");
-				SQLUpdate("Library","CREATE TABLE Author(AccNo text,AuthorName text)");
-				SQLUpdate("Library","INSERT INTO BookDetails VALUES('"+col[0]+"','"+col[1]+"','"+col[2]+"','"+col[3]+"',"+col[4]+","+col[5]+",'"+col[6]+"')");
-				for(int i = 7; i < col.length; i++) {
-					if(!col[i].equals("") && !col[i].equals("NULL"))
+			while(sc.hasNextLine()) {
+				String line = sc.nextLine();
+				String[] col = line.split(";");
+				System.out.println(col[0]);
+
+				SQLUpdate("Library","INSERT INTO BookDetails VALUES('"+col[0]+"','"+col[1]+"','"+col[2]+"','"+col[3]+"',"+col[4]+","+col[5]+")");
+				for(int i = 6; i < col.length; i++) {
+					if(!col[i].equals(""))
 						SQLUpdate("Library","INSERT INTO Author VALUES('"+col[0]+"','"+col[i]+"')");
 				}
 			}
-		
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -60,14 +62,19 @@ public class csvImporter {
         }
 
 	static void SQLUpdate(String name,String update) {
-                try(Connection conn = DriverManager.getConnection("jdbc:mysql://"+Credentials.ip+name,Credentials.username,Credentials.password)) {
+                try(Connection conn = DriverManager.getConnection("jdbc:mysql://"+Credentials.ip+"/"+name,Credentials.username,Credentials.password)) {
                         try(Statement stmt = conn.createStatement()) {
                                 stmt.executeUpdate(update);
                         } catch(SQLException e) {
-                                e.printStackTrace();
+                                System.out.println("--------");
+				try(FileWriter fw = new FileWriter(new File("log"), true)) {
+					fw.write(update);
+				} catch(Exception ex) {}
+				//e.printStackTrace();
                         }
                 } catch(SQLException e) {
-                	e.printStackTrace();
+                	System.out.println("--------");
+			//e.printStackTrace();
                 }
         }
 }
